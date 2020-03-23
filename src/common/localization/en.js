c6072c861f1be1845e93634a44de17cbfc920385
Elias Keis <13063245+elKei24@users.noreply.github.com>
2020-03-23 20:56:30 +0100

    MUR-21: localize app

---
+++ src/common/localization/en.js
@@ -0,0 +1,22 @@
+export default {
+  games: {
+    title: 'Games',
+    gamecard: {
+      id: 'ID: %{id}',
+      code: 'Code: %{code}'
+    }
+  },
+  credits: {
+    title: 'Credits',
+    content: 'Icon made by mavadee from www.flaticon.com'
+  },
+  addGame: {
+    title: 'Create game',
+    submit: 'Create game',
+    gameTitle: 'Title'
+  },
+  gamePreferences: {
+    dailyReassignment: 'Reassign victims daily',
+    allowAttestors: 'Allow attestors'
+  }
+}
---
+++ src/common/localization/translations.js
@@ -0,0 +1,9 @@
+// @flow
+
+import en from './en.js'
+import de from './de.js'
+
+export default {
+  en: en,
+  de: de
+}
