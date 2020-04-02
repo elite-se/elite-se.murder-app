564a90d9a9c59634e6d02306f4ca431604a2f723
Elias Keis <13063245+elKei24@users.noreply.github.com>
2020-04-02 12:46:56 +0200

    [MUR-49] inform server about notification token on app start

--- src/common/api/paths/userApi.js
+++ src/common/api/paths/userApi.js
@@ -1,30 +1,15 @@
 // @flow
 
-import { AUTH_HEADER, BACKEND } from '../api'
-import type { ApplicationUser } from '../../types/ApplicationUser'
-import ApiToken from '../../types/ApiToken'
-import { safeFetch } from '../safeFetch'
+import { BACKEND } from '../api'
+import authFetch from '../authFetch'
 
-const authBackend = `${BACKEND}/user`
+const userBackend = `${BACKEND}/user`
 
 export default class UserApi {
-  static signUp (user: ApplicationUser): Promise<Response> {
-    return safeFetch(`${authBackend}/sign-up`, {
-      method: 'POST',
-      body: JSON.stringify(user)
-    })
-  }
-
-  static login (user: ApplicationUser): Promise<ApiToken> {
-    return safeFetch(`${authBackend}/login`, {
-      method: 'POST',
-      body: JSON.stringify(user)
-    })
-      .then(response => {
-        const header = response.headers.get(AUTH_HEADER)
-        if (!header) { throw Error('login succeeded but still did not receive token') }
-        return header
-      })
-      .then(header => new ApiToken(header))
+  static setPushToken (token: string): Promise<Response> {
+    return authFetch(`${userBackend}/push-token`, {
+      method: 'PUT',
+      body: token
+    }, 'text/plain')
   }
 }
